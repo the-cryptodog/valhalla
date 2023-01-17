ead0bdef73172ca3ee2182ff30b3115132a2fad2
the-cryptodog <chenyuyu0515@gmail.com>
2023-01-17 16:46:02 +0800

    initial build

---
+++ app/src/main/java/com/app/valhalla/ui/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.app.valhalla.ui.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.app.valhalla.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
