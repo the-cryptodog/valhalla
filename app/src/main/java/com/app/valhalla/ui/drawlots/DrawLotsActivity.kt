166dc50d38d4b855f0fa95a9bddbf29d910bbd6d
林耿立 <kennylin80513@hotaimotor.com.tw>
2023-01-30 09:37:43 +0800

    加入搖籤筒動畫

--- app/src/main/java/com/app/valhalla/ui/drawlots/DrawLotsActivity.kt
+++ app/src/main/java/com/app/valhalla/ui/drawlots/DrawLotsActivity.kt
@@ -5,10 +5,16 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.core.content.res.ResourcesCompat
+import androidx.core.view.isVisible
+import androidx.lifecycle.lifecycleScope
 //import androidx.activity.viewModels
 import com.app.valhalla.R
 import com.app.valhalla.databinding.ActivityDrawLotsBinding
 import com.app.valhalla.util.FontUtil
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import pl.droidsonroids.gif.GifDrawable
 
 class DrawLotsActivity : AppCompatActivity() {
@@ -20,6 +26,21 @@ class DrawLotsActivity : AppCompatActivity() {
         setContentView(binding.root)
         var gifDrawable: GifDrawable = GifDrawable(resources, R.drawable.step_shake)
         binding.imgStepshake.setImageDrawable(gifDrawable)
-        binding.titleText.typeface=FontUtil.f_chinese_traditional(this)
+        binding.textTitleHeart.typeface=FontUtil.f_chinese_traditional(this)
+        binding.textTitleDivinationBlocks.typeface=FontUtil.f_chinese_traditional(this)
+
+        binding.imgDrawlots.setOnClickListener{
+            binding.viewDrawlots.isVisible = true
+            drawLotsViewModel.StepVisible()
+            drawLotsViewModel.getStepData.observe(this){
+                binding.imgStepshake.isVisible = drawLotsViewModel.getStepData.value == true
+                binding.imgStep.isVisible = drawLotsViewModel.getStepData.value != true
+                binding.textTitleDivinationBlocks.isVisible = drawLotsViewModel.getStepData.value != true
+            }
+        }
+
+        binding.textTitleDivinationBlocks.setOnClickListener {
+            
+        }
     }
 }
--- app/src/main/java/com/app/valhalla/ui/drawlots/DrawLotsViewModel.kt
+++ app/src/main/java/com/app/valhalla/ui/drawlots/DrawLotsViewModel.kt
@@ -1,6 +1,24 @@
 package com.app.valhalla.ui.drawlots
 
-import androidx.lifecycle.ViewModel
+import android.content.Context
+import androidx.lifecycle.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class DrawLotsViewModel: ViewModel() {
+    private val _saveStepVisible = MutableLiveData<Boolean>()
+    val getStepData: LiveData<Boolean> get() = _saveStepVisible
+
+
+    fun StepVisible(){
+        _saveStepVisible.value = true
+        viewModelScope.launch(Dispatchers.IO){
+            delay(3*1000)
+            withContext(Dispatchers.Main){
+                _saveStepVisible.value = false
+            }
+        }
+    }
 }
